1d2e4bc9f59c7f7786f5af241e29cda19253059a
刘程鑫 <756004757@qq.com>
2024-12-03 22:26:16 +0800

    update

--- src/Counter.sol
+++ src/Counter.sol
@@ -1,11 +1,19 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
+import {console2} from "forge-std/console2.sol";
+
 contract Counter {
     uint256 public number;
+    address public owner;
 
+    constructor(){
+        owner = msg.sender;
+    }
     function setNumber(uint256 newNumber) public {
         number = newNumber;
+        console2.log(owner);
+        console2.log("tsss");
     }
 
     function increment() public {
--- test/Counter.t.sol
+++ test/Counter.t.sol
@@ -1,8 +1,10 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import {Test, console} from "forge-std/Test.sol";
+import {Test} from "forge-std/Test.sol";
 import {Counter} from "../src/Counter.sol";
+import {console2} from "forge-std/console2.sol";
+import {Vm} from "forge-std/Vm.sol";
 
 contract CounterTest is Test {
     Counter public counter;
